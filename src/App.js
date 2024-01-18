cd54038fbbdba20697d3a5cc9a2dd859505b2393
sallah26 <143meski@gmail.com>
2024-01-18 15:57:58 +0300

    file and folder, project setup and others have been tackeld

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
+import "./index.css"
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div className="App bg-green-800">
+      <header className="App-header ">
+        
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <p className="text-3xl text-red-400 font-bold">
+          Edit <code>src/App.js</code> and save to reload. with selahadin
         </p>
         <a
           className="App-link"
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <p className='text-4xl text-gray-600 '>Na3vbar</p>
+  )
+}
+
+export default Navbar
