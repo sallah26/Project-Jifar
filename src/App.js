94a605a65742711bb851707bc11ce0b6ab127987
sallah26 <143meski@gmail.com>
2024-01-18 20:54:59 +0300

    navbar component is formed and updated

--- src/App.js
+++ src/App.js
@@ -7,21 +7,7 @@ import Navbar from './components/Navbar';
 function App() {
   return (
     <div className="App bg-green-800">
-      <header className="App-header ">
-        
-        <img src={logo} className="App-logo" alt="logo" />
-        <p className="text-3xl text-red-400 font-bold">
-          Edit <code>src/App.js</code> and save to reload. with selahadin
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
     </div>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,8 +2,41 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <p className='text-4xl text-gray-600 '>Na3vbar</p>
-  )
+    <nav className="text-4xl bg-slate-800">
+      <div className="flex justify-between p-4 font-light">
+        {/* <img src={} alt="" /> */}
+        <p className="text-3xl text-blue-500">Jifar LOGO</p>
+        <ul className="flex gap-10">
+          <li>
+            <a href="#login" className="text-sm text-white hover:bg-rounded ">
+              Services
+            </a>
+          </li>
+          <li>
+            <a href="#about" className="text-sm text-white">
+              CRM
+            </a>
+          </li>
+          <li>
+            <a href="#about" className="text-sm text-white">
+              Case Studies
+            </a>
+          </li>
+          <li>
+            <a href="#about" className="text-sm text-white">
+              Contact
+            </a>
+          </li>
+          <div className='gradient-wave-button rounded-full px-2'>
+          <button className='bg-gray-900 text-white text-2xl p-1 rounded-full'>
+            Contact us from
+          </button>
+          
+          </div>
+        </ul>
+      </div>
+    </nav>
+  );
 }
 
 export default Navbar
