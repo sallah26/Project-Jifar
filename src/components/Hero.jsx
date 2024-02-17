1ce4de98c11c88a153fa7df55131f46dfc741fe9
sallah26 <143meski@gmail.com>
2024-02-17 10:03:53 -0800

    msg

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -16,13 +16,13 @@ const Hero = () => {
   return (
     <main className="" id='hero'>
       <section className="container   dark:text-neutral-200 text-neutral-800 mx-auto px-4 flex flex-col lg:flex-row max-w-[1400px]">
-        <div className="py-4 lg:py-10 flex flex-col gap-1 lg:mt-20 items-start w-full lg:w-5/6">
+        <div className="py-4 lg:py-10 flex flex-col gap-2 lg:gap-1 lg:mt-20 items-start w-full lg:w-5/6">
           {/* <div  className="flex flex-col text-4xl font-semibold md:text-6xl lg:font-bold"> */}
-            <p data-aos="fade-up" data-aos-delay="700"  data-aos-offset="50" daa-aos-duration="400" className="text-4xl font-semibold md:text-6xl lg:font-bold">Smart Choices</p>
-            <p data-aos="fade-up" data-aos-delay="850" className=" text-4xl font-semibold md:text-6xl lg:font-bold  text-transparent bg-clip-text bg-gradient-to-r from-pink-500 to-purple-800">
+            <p data-aos="fade-up" data-aos-delay="700"  data-aos-offset="50" daa-aos-duration="400" className="text-5xl font-semibold md:text-6xl lg:font-bold">Smart Choices</p>
+            <p data-aos="fade-up" data-aos-delay="850" className=" text-5xl font-semibold md:text-6xl lg:font-bold  text-transparent bg-clip-text bg-gradient-to-r from-pink-500 to-purple-800">
                 Strong Results
             </p>
-              <p data-aos="fade-up" data-aos-delay="1000" className="text-4xl font-semibold md:text-6xl lg:font-bold">Choose Us</p>
+              <p data-aos="fade-up" data-aos-delay="1000" className="text-5xl font-semibold md:text-6xl lg:font-bold">Choose Us</p>
           {/* </div> */}
           <p data-aos="fade-up" data-aos-delay="1200" className="p-2  ">
             We Help Event Planning Service & Businesses Produce Spine-Chilling
@@ -36,8 +36,7 @@ const Hero = () => {
           </div>
         </div>
         <div className="p-6 lg:p-16" >
-          <img src={hero_bg} data-aos="fade-up" data-aos-delay="1000"  alt="hero image" />
-          
+          <img src={hero_bg} data-aos="fade-up" data-aos-delay="500" alt="hero image" />
         </div>
       </section>
     </main>
