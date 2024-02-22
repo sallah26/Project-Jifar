7fb4d2a8090d9d75ab9badcd3db3d15c96700a23
sallah26 <143meski@gmail.com>
2024-02-22 00:29:12 -0800

    calendly integration

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -3,8 +3,15 @@ import { InlineWidget } from "react-calendly";
 
 const App = () => {
   return (
-    <div className="App">
-      <InlineWidget url="https://calendly.com/nesredin" />
+    <div className="App p-5">
+      <div className="flex flex-col items-center ">
+        <p data-aos="fade-up" data-aos-delay="250" className="text-md lg:text-lg font-semibold bg-clip-text text-transparent bg-gradient-to-r from-[#ED7D31] to-[#a34f17]">
+        Let's Work Together
+        </p>
+        <p data-aos="fade-up" data-aos-delay="350" className="text-3xl lg:text-4xl dark:text-neutral-200">Book a Quick Call With Us?</p>
+        
+      </div>
+      <InlineWidget url="https://calendly.com/nesredin/welcome-to-jifar-agency-booking" />
     </div>
   );
 };
