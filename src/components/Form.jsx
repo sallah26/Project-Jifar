f085b58063deca44a733ad8184907ecf441791fe
sallah26 <143meski@gmail.com>
2024-02-22 01:11:00 -0800

    calendly integration

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -3,7 +3,7 @@ import { InlineWidget } from "react-calendly";
 
 const App = () => {
   return (
-    <div className="App p-5">
+    <div className="App h-auto ">
       <div className="flex flex-col items-center ">
         <p data-aos="fade-up" data-aos-delay="250" className="text-md lg:text-lg font-semibold bg-clip-text text-transparent bg-gradient-to-r from-[#ED7D31] to-[#a34f17]">
         Let's Work Together
